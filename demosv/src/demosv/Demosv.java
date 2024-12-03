fc39597244e3483259df5cebe5736ade85900f64
Nguyen Ba Trung <nbtrung1981@gmail.com>
2024-12-04 05:39:07 +0700

    cap nhat thêm hello

--- demosv/src/demosv/Demosv.java
+++ demosv/src/demosv/Demosv.java
@@ -14,6 +14,7 @@ public class Demosv {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hello");
         // TODO code application logic here
     }
     
